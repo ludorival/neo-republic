7456c6e18c0724bc8c765004526efc3f0460bb8d
Ludovic Dorival <ludorival@gmail.com>
2024-12-10 23:23:28 +0100

    Fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
-
-
 const nextConfig = {
-  /* config options here */
+  output: 'export', // Static HTML Export
+  images: {
+    unoptimized: true // Required for static export
+  },
+  // If you're using rewrites/redirects, you'll need to configure them here
 };
 
 export default nextConfig;

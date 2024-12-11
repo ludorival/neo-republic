ae3a9f015d77e88417400dabc093692fe31db8ff
Ludovic Dorival <ludorival@gmail.com>
2024-12-11 09:57:38 +0100

    Add apphosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 const nextConfig = {
-  output: 'export', // Static HTML Export
-  images: {
-    unoptimized: true // Required for static export
-  },
-  // If you're using rewrites/redirects, you'll need to configure them here
+  experimental: {
+		serverActions: true,
+	}
 };
 
 export default nextConfig;

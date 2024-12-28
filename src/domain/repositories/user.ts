061ec663282c2e3454b6642ba24f2b6a8b267fd8
Ludovic Dorival <ludorival@gmail.com>
2024-12-28 22:46:23 +0100

    Refactor repositories

--- src/domain/repositories/user.ts
+++ src/domain/repositories/user.ts
@@ -1,23 +1,5 @@
 import { User } from '@/domain/models/user';
-import { Database } from './database';
+import { CRUDRepository } from './baseRepository';
 
-export class UserRepository {
-  private readonly COLLECTION = 'users';
-
-  constructor(private readonly db: Database) {}
-
-  async createUser(user: User): Promise<void> {
-    await this.db.doc<User>(this.COLLECTION, user.id).set(user);
-  }
-
-  async getUser(userId: string): Promise<User | null> {
-    const docRef = this.db.doc<User>(this.COLLECTION, userId);
-    const exists = await docRef.exists();
-    if (!exists) return null;
-    return await docRef.data();
-  }
-
-  async updateUser(userId: string, updates: Partial<User>): Promise<void> {
-    await this.db.doc<User>(this.COLLECTION, userId).update(updates);
-  }
-} 
+export interface UserRepository extends CRUDRepository<string, User> {
+}
